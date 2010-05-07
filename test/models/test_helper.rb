58cb68964e53e716840754a1c1b0cc21244ff725
Elliot Winkler <elliot.winkler@gmail.com>
2010-05-06 23:55:20 -0500

    Delete the old test/ folder

--- test/models/test_helper.rb
+++
@@ -1,4 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
-Dir["#{TEST_DIR}/support/models/*.rb"].each {|file| require file }
-
-Protest.report_with(:documentation)
--- test/support/02_mocha.rb
+++
@@ -1,6 +0,0 @@
-#gem 'mcmire-mocha'
-#require 'mocha'
-# This must be required before matchy since matchy patches
-# the current test case's #run method, however mocha-protest-integration
-# completely overrides it
-#require 'mocha-protest-integration'
